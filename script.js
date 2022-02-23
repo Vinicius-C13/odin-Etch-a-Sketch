abc1a47ee17d16ec0ef0c66890a3970c48810f7f
vinicius <viniciuscosta.us@gmail.com>
2022-02-23 20:58:58 -0300

    added a erase button, a erase all button and a resize button

--- script.js
+++ script.js
@@ -1,7 +1,6 @@
 const container_div = document.getElementById('content-draw');
 const rangeValue_div = document.getElementById('range-value');
 const default_button = document.getElementById('default-button');
-const rainbow_button = document.getElementById('rainbow-button');
 const eraser_button = document.getElementById('eraser-button');
 const eraseAll_button = document.getElementById('eraseAll-button');
 const resize_button = document.getElementById('resize-button');
@@ -10,9 +9,15 @@ const resize_button = document.getElementById('resize-button');
 //send to createPixel function.
 
 function getPixelDimension() {
-    const boardDimension = Number(prompt('Choose the board dimension:'))
-    const pixelDimension = `${480/boardDimension}px`
-    createBoard(boardDimension, pixelDimension)
+    const boardDimension = Number(prompt('Choose the board size:\n(Max = 100)'));
+    const pixelDimension = `${480/boardDimension}px`;
+    
+    if(boardDimension > 0 & boardDimension <= 100) {
+        createBoard(boardDimension, pixelDimension);
+    }
+    else{
+        createBoard(16,'30px');
+    }
 }
 
 //Creates the square divs that fill the big white board with the takeDimension function.
@@ -28,30 +33,50 @@ function createBoard(bD, pD){
 
 getPixelDimension()
 
-//This function serves to resize the board
-
-//resize_button.addEventListener('click', function(){
-//    for(i=0;i<pixelsArray.length; i++){
-//    container_div.removeChild(pixelsArray[i]);
-//    }
-//    getPixelDimension();
-//})
-
-
 
 //Get all little square divs and add a event listener to them. if the mouse pass over,
 //the background color changes to black.
 const pixelsArray = [].slice.call(document.getElementsByClassName('pixel-style'));
 
+function changecolor(cursorcolor){
 pixelsArray.forEach(function(elem) {
     elem.addEventListener("mouseover", function(e) {
         if(e.buttons === 0) {
-            elem.style.backgroundColor = 'black';
-            console.log(e.buttons)
+            elem.style.backgroundColor = cursorcolor;
         }
         else{
         }
         
     });
-})
+});
+};
 
+//Calls the changecolor function with the default black color when the page loads.
+changecolor('black')
+
+
+
+//This function serves to resize the board
+resize_button.addEventListener('click', function(){
+    window.location.reload();
+});
+
+//This button serves to erase all the board
+eraseAll_button.addEventListener('click', function(){
+    pixelsArray.forEach(function(elem) {
+    elem.style.backgroundColor = 'white';
+    });
+});
+
+//This button serves to erase all the board
+eraser_button.addEventListener('click', function(){
+    default_button.style.backgroundColor = '#EFEFEF'
+    eraser_button.style.backgroundColor = '#04AA6D'
+    changecolor('white');
+});
+
+default_button.addEventListener('click', function(){
+    default_button.style.backgroundColor = '#04AA6D'
+    eraser_button.style.backgroundColor = '#EFEFEF'
+    changecolor('black');
+});
